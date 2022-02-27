66c0dc76434c269603e1214052ae5a252a99433d
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-27 13:10:11 +0530

    resume render

--- src/Components/Pages/Resume.js
+++ src/Components/Pages/Resume.js
@@ -7,8 +7,9 @@ export const Resume = () => {
         <h1>My <span>Resume</span></h1><br />
         <h3>Click <a href={resume} download="aadesh_resume"><span>Here</span></a> to download</h3><br />
         <div>
-        <object width="90%" height="2000px" data={resume}>Can not render resume on your browser</object> 
-        {/* <embed src={resume} width="90%" height="2000vh" /> */}
+          <h6>Rendering here if browser supports</h6><br/>
+        {/* <object width="90%" height="2000px" data={resume}>Can not render resume on your browser</object>  */}
+        <embed src={resume} width="90%" height="2000px"></embed>
         </div>
     </div>
   )
